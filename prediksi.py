3ba7ebf40e628d2af4a3c3392d338a6f95069a95
Ronald Wicaksono <67936558+ronald-afk@users.noreply.github.com>
2023-12-02 04:12:31 +0700

    Update prediksi.py

--- prediksi.py
+++ prediksi.py
@@ -1,7 +1,12 @@
 import pickle
 import streamlit as st
 
-model = pickle.load(open('prediksi_ph.sav','rb'))
+try:
+    # Cek apakah file model ada di direktori yang benar
+    model = pickle.load(open('prediksi_ph.sav', 'rb'))
+except Exception as e:
+    st.error(f"Error loading the model: {e}")
+    st.stop()
 
 st.title('Prediksi pH')
 
@@ -9,14 +14,17 @@ Hardness = st.number_input('Input Hardness')
 Alkalinity = st.number_input('Input Alkalinity')
 CarbonateRoot = st.number_input('Input Carbonate Root')
 Nitrate = st.number_input('Input Nitrate')
-Nitrite	= st.number_input('Input Nitrite')
+Nitrite = st.number_input('Input Nitrite')
 Freeclorine = st.number_input('Input Free clorine')
 Temperature = st.number_input('Input Temperature')
-DissolvedOxygen = st.number_input('Input Dissolved Oxygen')  # Fixed input name
+DissolvedOxygen = st.number_input('Input Dissolved Oxygen')  
 
 predict = ''
 
 if st.button('Prediksi pH'):
     # Pastikan nilai-nilai ini sesuai dengan format yang diharapkan oleh model
-    predict = model.predict([[Hardness, Alkalinity, CarbonateRoot, Nitrate, Nitrite, Freeclorine, Temperature, DissolvedOxygen]])
-    st.write('Prediksi pH:', predict)
+    if hasattr(model, 'predict'):
+        predict = model.predict([[Hardness, Alkalinity, CarbonateRoot, Nitrate, Nitrite, Freeclorine, Temperature, DissolvedOxygen]])
+        st.write('Prediksi pH:', predict)
+    else:
+        st.error("Model tidak memiliki metode 'predict'.")
